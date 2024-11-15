57b71db0dd23d4025da20bf39102efebb79678f4
Ayush Jain <ayushjain2205@gmail.com>
2024-11-16 00:51:17 +0700

    Config Londrina

--- npc.ai/src/pages/_document.tsx
+++ npc.ai/src/pages/_document.tsx
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Londrina+Solid:wght@100;300;400;900&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
